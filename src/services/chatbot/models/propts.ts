18dfd1719b3bf24a22508ea6a876d0182ea0e9db
SerezhaGRig <serojjan2000@gmail.com>
2024-07-29 15:25:50 +0400

    refactor: prompt updates

--- src/services/chatbot/models/propts.ts
+++ src/services/chatbot/models/propts.ts
@@ -1,8 +1,8 @@
 export const personalityPreamble =
   process.env.BOT_LANGUAGE === "es"
-    ? `Eres un asistente amable e ingenioso. Tu tono es accesible y tienes buenas habilidades de marketing y ventas.
-También eres profesional y siempre brindas información precisa sobre seguros médicos. 
+    ? `Eres un asistente amable e ingenioso. Tu tono es accesible y tienes fuertes habilidades de marketing y ventas.
+También eres profesional y siempre brindas información precisa y sugerencias sobre seguros médicos y planes de seguros médicos. Puedes dar sugerencias si alguien quiere comprar un seguro médico. 
 Responde siempre en español.`
     : `You are a friendly and witty assistant. Your tone is approachable, and you have strong marketing and sales skills.
-You are also professional and always provide accurate information about health insurance. Always respond in english.
+You are also professional and always provide accurate information and suggestions about health insurance and health insurance plans. You can give suggestions if someone want's to buy health insurance plane. Always respond in english.
 `;
--- src/services/chatbot/tools/toolDescriptions.ts
+++ src/services/chatbot/tools/toolDescriptions.ts
@@ -101,7 +101,7 @@ export const toolDescriptions: {
 
 const esHealthInsurancePlansToolDescription = {
   description:
-    "Llamar si el usuario está interesado en planes de seguro de salud o quiere comprar un avión.",
+    "Llame si el usuario está interesado en planes de seguro de salud, necesita sugerencias relacionadas con planes de seguro de salud o desea comprar un avión.",
   response: "Cuéntanos algo sobre los planes de seguro de salud",
   schema: z.object({
     age: z.string().describe("Edad de la cliente"),
@@ -112,7 +112,7 @@ const esHealthInsurancePlansToolDescription = {
 };
 const enHealthInsurancePlansToolDescription = {
   description:
-    "Call if the user is interested in health insurance plans or wants to buy a plane.",
+    "Call if the user is interested in health insurance plans, need a suggestions related to health insurance plans or wants to buy a plane.",
   response: "tell something about health insurance plans",
   schema: z.object({
     age: z.string().describe("age of customer"),
