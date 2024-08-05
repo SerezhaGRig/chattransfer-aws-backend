8cb2961e81bbffeef9b5bc2820a6e4693445f472
SerezhaGRig <serojjan2000@gmail.com>
2024-08-05 16:33:39 +0400

    refactor: enables health insurance plan tool

--- src/services/chatbot/tools/index.ts
+++ src/services/chatbot/tools/index.ts
@@ -2,6 +2,11 @@ import { ToolNode } from "@langchain/langgraph/prebuilt";
 import { BaseMessage } from "@langchain/core/messages";
 import { retrieverTools } from "./vectorStoreRetrieverTool";
 import { connectWithAgent } from "./connectWithAgent";
+import { healthInsurancePlans } from "./healthInsurancePlans";
 
-export const tools = [...retrieverTools, connectWithAgent];
+export const tools = [
+  ...retrieverTools,
+  connectWithAgent,
+  healthInsurancePlans,
+];
 export const toolNode = new ToolNode<{ messages: BaseMessage[] }>(tools);
